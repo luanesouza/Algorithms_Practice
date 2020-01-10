c3ea0827ead3f969f4c331b07ed77ea02f439a65
luanesouza <luanesouza411@gmail.com>
2020-01-10 12:57:07 -0500

    palindrome solution done

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,22 @@
+// function isPalindrome(string) {
+//
+//   const reversedString = string.split('').reverse().join('')
+//
+//   if(reversedString === string) {
+//     console.log(true);
+//     return true;
+//   } else {
+//     console.log(false);
+//     return false
+//   }
+// }
+
 function isPalindrome(string) {
-
-  const reversedString = string.split('').reverse().join('')
-
-  if(reversedString === string) {
-    console.log(true);
-    return true;
-  } else {
-    console.log(false);
-    return false
+  let reversed = '';
+  for(let element of string) {
+    reversed = element + reversed
   }
+  return string === reversed
 }
 
 isPalindrome('baby')
